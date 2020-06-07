9a952fc9b26d7a1cdb17ea3800907a8e46a9c9e0
mkunnari1 <mkunnari1@gmail.com>
2020-06-06 19:39:55 -0700

    Start Contact Manager Test

---
+++ ContactManager/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult CreateContact()
+        {
+            return View();
+        }
+
+        public IActionResult EditContact(int Id)
+        {
+            return View();
+            
+        }
+    }
+}
---
+++ ContactManager/ContactManager/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Company { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
