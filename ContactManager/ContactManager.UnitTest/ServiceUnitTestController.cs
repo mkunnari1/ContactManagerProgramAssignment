bac3780355b44db3aa07638911a79e1cfb8221bc
mkunnari1 <mkunnari1@gmail.com>
2020-06-11 19:20:07 -0700

    Unit Testing Is a No Go

---
+++ ContactManager/ContactManager.UnitTest/ServiceUnitTestController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactManager.UnitTest
+{
+    class ServiceUnitTestController
+    {
+        
+    }
+}
--- ContactManager/ContactManager.UnitTest/UnitTest1.cs
+++ ContactManager/ContactManager.UnitTest/UnitTest1.cs
@@ -1,14 +1,44 @@
 using System;
 using Xunit;
+using ContactManager.Services;
+using ContactManager.Models;
+using ContactManager.ViewModels;
+using ContactManager.Infrastructure;
 
 namespace ContactManager.UnitTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        //private readonly ContactManagerTestContext dbContext;
 
+        //public UnitTest1(ContactManagerTestContext dbContext)
+        //{
+        //    this.dbContext = dbContext;
+        //}
+        //[Fact]
+        //public void Test1()
+        //{
+        //    var list = new ContactList();
+        //    foreach (var contact in dbContext.Contacts)
+        //    {
+        //        list.Contacts.Add(new ContactList.Contact
+        //        {
+        //            Id = contact.Id,
+        //            Name = contact.Name,
+        //            Company = contact.Company,
+        //            Email = contact.Email,
+        //            PhoneNumber = contact.PhoneNumber
+        //        });
+        //    }
+        //    var expected = list.Contacts.Count;
+        //    var actual = 1;
+        //    Assert.Equal(expected, actual);
+        //}
+        
+        [Fact]
+        public void test1()
+        {
+            
         }
     }
 }
